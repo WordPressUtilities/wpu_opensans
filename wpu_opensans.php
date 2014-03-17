b326dcfab551e03b7a076e9bbbbeddcbd32fe45d
Grégory Viguier <Screenfeed@users.noreply.github.com>
2014-03-17 23:19:20 +0100

    Local open-sans for the login page.
    The login page does not use 'wp_enqueue_scripts', so the hook didn't work, we use 'login_init' instead. Uses $wp_styles directly, which must be initiated for the login. Also, check if the CSS src is local before editing it.

--- wpu_opensans.php
+++ wpu_opensans.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: WP Utilities Open Sans
 Description: Use an embedded version of the font Open Sans, and bypasses Google Fonts in WP Admin
-Version: 0.1.3
+Version: 0.2
 Author: Darklg
 Author URI: http://darklg.me/
 License: MIT License
@@ -11,7 +11,14 @@ License URI: http://opensource.org/licenses/MIT
 
 add_action( 'wp_enqueue_scripts', 'wpu_opensans_enqueue_style' );
 add_action( 'admin_enqueue_scripts', 'wpu_opensans_enqueue_style' );
+add_action( 'login_init', 'wpu_opensans_enqueue_style' );
+
 function wpu_opensans_enqueue_style() {
-    wp_deregister_style( 'open-sans' );
-    wp_register_style( 'open-sans', plugins_url( '/assets/css/open-sans.css', __FILE__ ) );
+	global $wp_styles;
+	if ( !is_a($wp_styles, 'WP_Styles') ) {
+		$wp_styles = new WP_Styles();
+	}
+	if ( !empty($wp_styles->registered['open-sans']) && strpos($wp_styles->registered['open-sans']->src, site_url()) !== 0 ) {
+		$wp_styles->registered['open-sans']->src = plugins_url( '/assets/css/open-sans.css', __FILE__ );
+	}
 }
